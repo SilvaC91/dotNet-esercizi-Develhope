fc340c94c624d54acf8c1e02797d5d7499206bd5
Silvano <silvanocarnevale91@gmail.com>
2023-11-21 22:14:01 +0100

    a

--- ConsoleApp1prov/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Inserisci il tuo nome");
-string nome = Console.ReadLine();
-
-if(nome.Length == 0){
-    Console.WriteLine("Nome non valido!");
-    Environment.Exit(1);
-}
-Console.WriteLine($"Hello, {nome}");
