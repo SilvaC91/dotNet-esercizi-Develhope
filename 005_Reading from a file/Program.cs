f4e6f508775d9cd73d5a1c3f22d9462bb066f491
Silvano <silvanocarnevale91@gmail.com>
2023-11-22 20:28:27 +0100

    reading from a file

---
+++ 005_Reading from a file/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Inserisci nome file");
+string filePath = Console.ReadLine();
+
+if(filePath.Length == 0){
+    Console.WriteLine("Nome file non valido!");
+    Environment.Exit(1);
+}
+
+string content = File.ReadAllText(filePath);
+Console.WriteLine(content);
