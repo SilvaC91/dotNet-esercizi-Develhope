ce2c045500f454310507bc329fe59cdbf0cb45c9
Silvano <silvanocarnevale91@gmail.com>
2023-11-22 10:18:59 +0100

    sum of two numbers

---
+++ 003_sum of two numbers in input/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Inserisci il primo numero");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Inserisci il secondo numero");
+int num2 = int.Parse(Console.ReadLine());
+
+int sum = num1 + num2;
+Console.WriteLine($"{num1} + {num2} = {sum}");
