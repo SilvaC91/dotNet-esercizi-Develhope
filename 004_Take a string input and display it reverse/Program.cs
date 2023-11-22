cdb76018e491d253ca6403dc9569a9e7ebdc63fb
Silvano <silvanocarnevale91@gmail.com>
2023-11-22 10:40:14 +0100

    reverse a string

---
+++ 004_Take a string input and display it reverse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Inserisci il testo");
+string str = Console.ReadLine();
+
+if(str.Length == 0){
+    Console.WriteLine("Nessun testo inserito!");
+    Environment.Exit(1);
+}
+
+Console.WriteLine(string.Concat(Enumerable.Reverse(str)));
