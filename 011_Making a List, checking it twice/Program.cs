535349593568a4b53a105f515de7779c4de4fb5d
Silvano <silvanocarnevale91@gmail.com>
2023-11-25 06:48:03 +0100

    Making a List, checking it twice

---
+++ 011_Making a List, checking it twice/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Create a console application. The console application should ask the user 10 times for input. 
+This input is added to a list. After the 10th time, the console program will print out the list twice. 
+Once from start to finish, the other time in reverse. */
+
+Console.WriteLine("Insert 10 numbers");
+
+List<int> numbers = new List<int>();
+
+for(int i=0; i<10; i++){
+    numbers.Add(int.Parse(Console.ReadLine()));
+}
+
+Console.WriteLine("List:");
+foreach(int number in numbers){
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("\nReversed list:");
+foreach(int number in numbers){
+    Console.WriteLine(number);
+}
+
+
+
