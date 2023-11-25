59eea33ee2fd4181d82a0842ce8f64dd6125943a
Silvano <silvanocarnevale91@gmail.com>
2023-11-25 17:20:28 +0100

    Taking the train

---
+++ 015_Taking the train/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Create a console application that uses a LinkedList to represent a train route with various stations.
+Add and remove stations, and display the route. Can you make the train station into a class?
+*/
+
+
+using _015_Taking_the_train;
+
+LinkedList<TrainStation> trainRoute_Como_to_Milan = new LinkedList<TrainStation>();
+
+TrainStation s1 = new TrainStation("Como S. Giovanni");
+TrainStation s2 = new TrainStation("Camerlata");
+TrainStation s3 = new TrainStation("Seregno");
+TrainStation s4 = new TrainStation("Monza");
+TrainStation s5 = new TrainStation("Milano Centrale");
+
+
+trainRoute_Como_to_Milan.AddFirst(s5);
+trainRoute_Como_to_Milan.AddFirst(s4);
+trainRoute_Como_to_Milan.AddFirst(s3);
+trainRoute_Como_to_Milan.AddFirst(s2);
+trainRoute_Como_to_Milan.AddFirst(s1);
+
+Console.WriteLine("Train route from Como to Milan:");
+foreach(TrainStation station in trainRoute_Como_to_Milan){
+    Console.WriteLine(station.name);
+}
+
---
+++ 015_Taking the train/TrainStation.cs
@@ -0,0 +1,10 @@
+﻿namespace _015_Taking_the_train;
+
+public class TrainStation
+{
+    public string name {get; set;}
+
+    public TrainStation(string name){
+        this.name = name;
+    }
+}
