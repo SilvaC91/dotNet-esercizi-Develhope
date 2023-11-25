4aa8e51b9135680c4401ae987617637ca9d73b8e
Silvano <silvanocarnevale91@gmail.com>
2023-11-25 04:39:08 +0100

    Celsius to Fahrenheit conversion

---
+++ 009_Celsius to Fahrenheit conversion/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Write your temperature in Celsius");
+string inputValue = Console.ReadLine();
+
+if(inputValue.Length == 0){
+    Console.WriteLine("Invalid value!");
+    Environment.Exit(1);
+}
+
+double temperatureCelsius = double.Parse(inputValue);
+double temperatureFahrenheit = (temperatureCelsius * 1.8) + 32;
+Console.WriteLine($"{temperatureCelsius} Celsius are {temperatureFahrenheit} Fahrenheit");
+
+
+
+
+
+
+
