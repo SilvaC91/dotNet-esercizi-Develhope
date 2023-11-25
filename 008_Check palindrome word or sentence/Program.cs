2ab87e36a5592a9359d2ec3015f33bfab2a21e85
Silvano <silvanocarnevale91@gmail.com>
2023-11-25 04:17:11 +0100

    Ckeck palindrome word or sentence

---
+++ 008_Check palindrome word or sentence/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Write a word or a sentence");
+string str = Console.ReadLine();
+
+if(str.Length == 0){
+    Console.WriteLine("Invalid sentence!");
+    Environment.Exit(1);
+}
+
+string reversedString = null;
+
+for(int i=str.Length-1; i>=0; i--){
+    reversedString += str.Substring(i, 1);
+}
+
+
+if(str.Replace(" ","") == reversedString.Replace(" ","")){
+    Console.WriteLine("Your sentence is a palindrome!");
+} else {
+    Console.WriteLine("Your sentence is not a palindrome :(");
+}
