879f9a806ba6e387ca8f4489f4442de57343a02b
Silvano <silvanocarnevale91@gmail.com>
2023-11-27 21:30:41 +0100

    Sharing victory

---
+++ 020_Sharing victory/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Edit the collection of students from previous exercise, Grading the students. 
+Create two students with the same grade, that grade should be the highest. How does your application deal with that?
+If needed, change the application to allow multiple students to have the highest grade.
+Extra points if you can print out their names in alphabetical order.
+*/
+
+using System.Collections;
+using _020_Sharing_victory;
+
+List<Student> students = new List<Student>(){new Student("Marcello", 68.72),
+                                             new Student("Claudio", 98.9),
+                                             new Student("Simona", 60.6),
+                                             new Student("Giorgia", 77.32),
+                                             new Student("Francesco", 98.9),
+                                             new Student("Sara", 90.5),
+                                             new Student("Bruna", 81.22),
+                                             new Student("Lucia", 98.9),
+                                             new Student("Pietro", 72.45),
+                                             new Student("Alessandro", 98.9)
+                                            };
+
+
+double gradeAverage = 0;
+for(int i=0; i<students.Count; i++){
+    gradeAverage += students[i].Grade;
+}
+gradeAverage /= students.Count;
+Console.WriteLine($"Grade average = {gradeAverage}");
+
+double gradeMax = -1;
+int indexBestStudent = 0;
+List<Student> bestStudents = new List<Student>();
+for(int i=0; i<students.Count; i++){
+    if(students[i].Grade > gradeMax){
+        indexBestStudent = i;
+        }
+    gradeMax = Math.Max(students[i].Grade, gradeMax);
+}
+for(int i=0; i<students.Count; i++){
+    if(students[i].Grade == gradeMax){
+        bestStudents.Add(students[i]);
+    }
+}
+bestStudents.Sort((x, y) => x.Name.CompareTo(y.Name));
+Console.Write($"Student with the highest grade\nName: ");
+foreach(Student student in bestStudents){
+    Console.Write($"{student.Name}, ");
+}
+Console.WriteLine($"Grade: {gradeMax}");    
---
+++ 020_Sharing victory/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace _020_Sharing_victory;
+
+public class Student
+{
+    public string Name {get; set;}
+    public double Grade {get; set;}
+
+    public Student(string name, double grade){
+        this.Name = name;
+        this.Grade = grade;
+    }
+}
