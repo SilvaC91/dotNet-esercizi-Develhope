7dda64ebc0cbd04ffcf7a0900c69728097bf815e
Silvano <silvanocarnevale91@gmail.com>
2023-11-27 07:58:48 +0100

    017_Explore the HashSet

---
+++ 017_Explore the HashSet/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Create a HashSet<string>. Explore what happens when you add unique values to it. 
+Does a HashSet consider strings with different capitalisations unique?
+*/
+
+
+HashSet<string> myHashSet = new HashSet<string>();
+
+myHashSet.Add("dog");
+myHashSet.Add("dog");
+myHashSet.Add("Dog");
+
+foreach(string str in myHashSet){
+    Console.WriteLine(str);
+}
+
+/*
+output is:
+dog
+Dog
+
+so HashSet is case sensitive
+*/
