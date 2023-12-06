7b2fae096af9906d959e3c2db1c719330f9bdf34
Silvano <silvanocarnevale91@gmail.com>
2023-12-06 11:23:03 +0100

    Sorted

---
+++ 023_Sorted/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Create another list of 1000 random integers. Can you sort them from low to high? Print out the result.
+Bonus points if you can do it in reverse too.
+*/
+
+Random random = new Random();
+IEnumerable<int> range = Enumerable.Range(0, 10)
+                      .Select(i => random.Next(0, 501));
+
+List<int> numbers = new List<int>(range);
+
+var sortedNumbers = from number in numbers
+                    orderby number
+                    select number;
+
+var descendingSortedNumbers = from number in numbers
+                    orderby number descending
+                    select number;
+
+Console.WriteLine("Ascending sorted numbers");
+foreach(int number in sortedNumbers){
+    Console.WriteLine(number);
+}
+Console.WriteLine("\nDescending sorted numbers");
+foreach(int number in descendingSortedNumbers){
+    Console.WriteLine(number);
+}
